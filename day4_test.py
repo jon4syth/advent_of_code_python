467c8e452caf1f4fa859514dc19460018638ce7d
N8 Rollins <partiesofsolace@gmail.com>
2020-12-11 13:32:56 +0800

    Add second test

--- day4_test.py
+++ day4_test.py
@@ -1,6 +1,8 @@
 import unittest
 from day4 import Day4
 
+
+
 class TestDay4(unittest.TestCase):
     def test_read_file(self):
         """
@@ -10,6 +12,25 @@ class TestDay4(unittest.TestCase):
 
         self.assertEqual("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n", input[0])
 
+    def test_has_two_valid_passports(self):
+
+        """
+        testing to see if there are 2 valid passports in the data
+
+        byr (Birth Year)
+    iyr (Issue Year)
+    eyr (Expiration Year)
+    hgt (Height)
+    hcl (Hair Color)
+    ecl (Eye Color)
+    pid (Passport ID)
+    cid (Country ID)
+        :return:
+        """
+        lines = Day4.read_file("input")
+        count = Day4.count_valid_passports(lines)
+
+        self.assertEqual(2, count)
 
 if __name__ == '__main__':
     unittest.main()
